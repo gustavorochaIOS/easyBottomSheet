e7a82faf69247191be0181c4200dade4f8078dcd
Gustavo Rocha <gustavofrocha7@gmail.com>
2022-02-13 17:56:24 -0300

    fix bugs

--- easyBottomSheet/easyBottomSheet/GenderSheetView.swift
+++ easyBottomSheet/easyBottomSheet/GenderSheetView.swift
@@ -18,11 +18,11 @@ class GenderSheetView: UIView {
         animatetUI(true)
     }
     
-    private func animatetUI(_ isPresent: Bool = false) {
-        self.tag = 01
+    private func animatetUI(_ isPresent: Bool = false, bottomHeight: CGFloat = 0) {
+        self.tag = 1
         UIView.animate(withDuration: 0.25) {
             self.backgroundColor = UIColor.black.withAlphaComponent(isPresent ? 0.7 : 0)
-            self.constBottomHeight.constant = isPresent ? 0 : -300
+            self.constBottomHeight.constant = isPresent ? 0 : bottomHeight
             self.layoutIfNeeded()
         } completion: {_ in
             if !isPresent {
@@ -33,7 +33,7 @@ class GenderSheetView: UIView {
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first {
-            if touch.view?.tag == 01 {
+            if touch.view?.tag == 1 {
                 animatetUI()
             }
         }
--- easyBottomSheet/easyBottomSheet/ViewController.swift
+++ easyBottomSheet/easyBottomSheet/ViewController.swift
@@ -13,22 +13,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
-        
     }
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+    
+    @IBAction func showSheet(_ sender: Any) {
+        DispatchQueue.main.async {
             self.showGenderSheet()
         }
     }
     
     private func showGenderSheet() {
-        view.endEditing(true)
         genderSheet = getInstanceFromGenderSheet()
-        navigationController?.interactivePopGestureRecognizer?.isEnabled = false
-        navigationController?.view.addSubview(genderSheet)
+        view.addSubview(genderSheet)
         setupGenderSheetConstraints()
         genderSheet.didDismiss = {[weak self] in
             self?.genderSheet.removeFromSuperview()
@@ -44,8 +39,8 @@ class ViewController: UIViewController {
     private func setupGenderSheetConstraints() {
         genderSheet.translatesAutoresizingMaskIntoConstraints = false
         genderSheet.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        genderSheet.leftAnchor.constraint(equalTo: view.readableContentGuide.leftAnchor).isActive = true
-        genderSheet.rightAnchor.constraint(equalTo: view.readableContentGuide.rightAnchor).isActive = true
+        genderSheet.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        genderSheet.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         genderSheet.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
     }
 }
