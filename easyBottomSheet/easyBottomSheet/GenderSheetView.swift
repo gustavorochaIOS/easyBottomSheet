f154895903c1f8a70cfa8d8e2d2ffdea15f3a7d0
Gustavo Rocha <gustavo.rocha@usemobile.xyz>
2022-02-13 16:23:22 -0300

    created layout

--- easyBottomSheet/easyBottomSheet/GenderSheetView.swift
+++ easyBottomSheet/easyBottomSheet/GenderSheetView.swift
@@ -9,6 +9,33 @@ import UIKit
 
 class GenderSheetView: UIView {
 
+    var didDismiss: (() -> Void)?
     
+    @IBOutlet weak var constBottomHeight: NSLayoutConstraint!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        animatetUI(true)
+    }
+    
+    private func animatetUI(_ isPresent: Bool = false) {
+        self.tag = 01
+        UIView.animate(withDuration: 0.25) {
+            self.backgroundColor = UIColor.black.withAlphaComponent(isPresent ? 0.7 : 0)
+            self.constBottomHeight.constant = isPresent ? 0 : -300
+            self.layoutIfNeeded()
+        } completion: {_ in
+            if !isPresent {
+                self.didDismiss?()
+            }
+        }
+    }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            if touch.view?.tag == 01 {
+                animatetUI()
+            }
+        }
+    }
 }
--- easyBottomSheet/easyBottomSheet/ViewController.swift
+++ easyBottomSheet/easyBottomSheet/ViewController.swift
@@ -8,11 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var genderSheet: GenderSheetView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.showGenderSheet()
+        }
+    }
+    
+    private func showGenderSheet() {
+        view.endEditing(true)
+        genderSheet = getInstanceFromGenderSheet()
+        navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+        navigationController?.view.addSubview(genderSheet)
+        setupGenderSheetConstraints()
+        genderSheet.didDismiss = {[weak self] in
+            self?.genderSheet.removeFromSuperview()
+        }
+    }
+    
+    private func getInstanceFromGenderSheet() -> GenderSheetView {
+        let bundle = Bundle(for: ViewController.self)
+        let views = UINib(nibName: "GenderSheetView", bundle: bundle).instantiate(withOwner: nil, options: nil) as! [UIView]
+        return views.first as! GenderSheetView
+    }
+    
+    private func setupGenderSheetConstraints() {
+        genderSheet.translatesAutoresizingMaskIntoConstraints = false
+        genderSheet.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        genderSheet.leftAnchor.constraint(equalTo: view.readableContentGuide.leftAnchor).isActive = true
+        genderSheet.rightAnchor.constraint(equalTo: view.readableContentGuide.rightAnchor).isActive = true
+        genderSheet.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
 }
 
